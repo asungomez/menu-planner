9481cecbc88d358b80d15cd3e82a089b652d24c6
Asun Gomez <agcastro7@gmail.com>
2020-12-30 14:27:22 +0100

    Force deploy

--- amplifyPush.sh
+++ amplifyPush.sh
@@ -18,17 +18,17 @@ init_env () {
     CATEGORIES=$6
 
     echo "# Start initializing Amplify environment: ${ENV}"
-    STACKINFO="$(amplify env get --json --name ${ENV})"
-    envCache --set stackInfo ${STACKINFO}
     if [[ -z ${STACKINFO} ]];
     then
         STACKINFO="$(amplify env get --json --name ${ENV})"
         envCache --set stackInfo ${STACKINFO}
         echo "STACKINFO="${STACKINFO}
-        # echo "# Initializing new Amplify environment: ${ENV} (amplify init)"
-        # [[ -z ${CATEGORIES} ]] && amplify init --amplify ${AMPLIFY} --providers ${PROVIDERS} --codegen ${CODEGEN} --yes || amplify init --amplify ${AMPLIFY} --providers ${PROVIDERS} --codegen ${CODEGEN} --categories ${CATEGORIES} --yes
-        # echo "# Environment ${ENV} details:"
-        # amplify env get --name ${ENV}
+        echo "# Importing Amplify environment: ${ENV} (amplify env import)"
+        amplify env import --name ${ENV} --config "${STACKINFO}" --awsInfo ${AWSCONFIG} --yes;
+        echo "# Initializing new Amplify environment: ${ENV} (amplify init)"
+        [[ -z ${CATEGORIES} ]] && amplify init --amplify ${AMPLIFY} --providers ${PROVIDERS} --codegen ${CODEGEN} --yes || amplify init --amplify ${AMPLIFY} --providers ${PROVIDERS} --codegen ${CODEGEN} --categories ${CATEGORIES} --yes
+        echo "# Environment ${ENV} details:"
+        amplify env get --name ${ENV}
     else
         echo "STACKINFO="${STACKINFO}
         echo "# Importing Amplify environment: ${ENV} (amplify env import)"
