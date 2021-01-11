a31b3f952dc432629a9ff4d00f13ba02b9b9df7a
Asun Gomez <agcastro7@gmail.com>
2021-01-11 17:04:13 +0100

    Delete unused vars

--- src/routers/AuthenticatedRoute.jsx
+++ src/routers/AuthenticatedRoute.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Redirect, Route, useLocation } from 'react-router-dom';
+import { Redirect, Route } from 'react-router-dom';
 
 import { useAuthenticationContext } from '../contexts/AuthenticationContext';
 
